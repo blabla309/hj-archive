cd99bae8b84a3460ff7117a013d610ee8a7cbfc4
blabla309 <blabla309@naver.com>
2018-10-31 16:06:57 +0900

    Pdo Test

--- public_html/test/dao_test.php
+++ public_html/test/dao_test.php
@@ -3,7 +3,7 @@
  * User: 김형주
  * Date: 2018-10-24
  */
-
+require_once "/home/". get_current_user() . "/init/conf/def.php";
 require_once _LIB_HOM_DIR . '/Dao.php';
 
 Dao::getInstance("db_hj_archive_01");
