75e3f0fba3fa4e9f879a4dd09cc34645699277fa
blabla309 <blabla309@naver.com>
2018-12-27 16:55:50 +0900

    Working Router

--- controller/Controller.php
+++ controller/Controller.php
@@ -7,8 +7,21 @@
 
 
 class Controller{
+    protected $method;
+    protected $path;
+    protected $queryString;
+
     public function __construct(){
-        print_r($_SERVER);
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $this->queryString = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+
+        print <<<EOM
+$this->method <br/>
+$this->path <br/>
+$this->queryString <br/>
+
+EOM;
     }
 }
 
--- public_html/index.php
+++ public_html/index.php
@@ -9,24 +9,7 @@ require_once _CONTROLLER_DIR . "/FrontController.php";
 
 class Index{
   public function __construct(){
-    #TEST
-    $requestType = $_SERVER['REQUEST_METHOD'];
-
-    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $query = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
-
     new FrontController();
-
-    /*
-
-    $path_arr = explode('/', $path);
-    print <<<EOM
-Path: {$path} <br/>
-Query: {$query} <br/>
-Request Type : $requestType
-
-EOM;*/
-
   }
 }
 
